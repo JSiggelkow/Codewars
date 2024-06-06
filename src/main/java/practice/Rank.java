de9552f4d941c6bab5526ef85dc72e0cb2873f7d
JSiggelkow <J.Siggelkow@iks-gmbh.com>
2024-06-06 16:30:03 +0200

    Add solves Prize Draw Practice 6kyu

---
+++ src/main/java/practice/Rank.java
@@ -0,0 +1,39 @@
+package practice;
+
+import java.util.*;
+
+record Person(String name, int winningNum) {
+
+}
+
+public class Rank {
+
+	public static String nthRank(String st, Integer[] we, int n) {
+
+		if (st.isEmpty()) return "No participants";
+		String[] names = st.split(",");
+
+		if (names.length < n) return "Not enough participants";
+
+		ArrayList<Person> persons = new ArrayList<>();
+		for (int i = 0; i < names.length; i++) {
+			persons.add(new Person(names[i], getWinningNum(names[i], we[i])));
+		}
+
+		return persons.stream()
+				.sorted(Comparator.comparing(Person::winningNum).reversed()
+						.thenComparing(Person::name))
+				.skip(n - 1)
+				.map(Person::name)
+				.findFirst()
+				.orElse(null);
+	}
+
+	private static int getWinningNum(String st, int weight) {
+		int winningNum = st.length();
+		for (int i = 0; i < st.length(); i++) {
+			winningNum += st.toLowerCase().charAt(i) - 96;
+		}
+		return winningNum * weight;
+	}
+}
