e62528be089e433e5b12eb7d4b217dc2b017c9bf
JSiggelkow <J.Siggelkow@iks-gmbh.com>
2024-06-07 12:45:09 +0200

    Add 'isSatorSquare' functionality and tests
    Implemented 'isSatorSquare' method for evaluating 'Sator Squares' in Java. This method checks the content of a 2D array for the properties of a Sator Square. Corresponding test cases have also been added.

---
+++ src/main/java/practice/is_sator_square/Kata.java
@@ -0,0 +1,84 @@
+package practice.is_sator_square;
+
+public class Kata {
+
+	public static Boolean isSatorSquare(char[][] tablet) {
+
+
+		int wordCount = tablet.length;
+		int upperRight = 0;
+		int lowerLeft = 0;
+		int upperLeft = 0;
+		int lowerRight = 0;
+
+		//Calculate upper right
+		for (int i = 0; i < wordCount; i++) {
+			for (int j = 0; j < wordCount - i; j++) {
+				upperRight += tablet[i][j + i] - 64;
+			}
+		}
+		//Calculate lower left
+		for (int i = wordCount; i > 0; i--) {
+			for (int j = 0; j < i ; j++) {
+				lowerLeft += tablet[i - 1][j] - 64;
+			}
+		}
+		//Calculate upper left
+		for (int i = 0; i < wordCount; i++) {
+			for (int j = 0; j < wordCount - i; j++) {
+				upperLeft += tablet[i][j] - 64;
+			}
+		}
+		//Calculate lower right
+		for (int i = 0; i < wordCount; i++) {
+			for (int j = wordCount; j >= wordCount - i ; j--) {
+				lowerRight += tablet[i][j - 1] - 64;
+			}
+		}
+
+		//check Top to bottom
+		String[] names = getWords(tablet, wordCount);
+		boolean topToBottom = checkTopToBottom(names, tablet, wordCount);
+		return (upperRight == lowerLeft) && (upperLeft == lowerRight) && topToBottom;
+
+
+	}
+
+	public static boolean checkTopToBottom(String[] words, char[][] tablet, int wordCount) {
+		boolean isValid = true;
+
+		for (String word : words) {
+			boolean isWordFound = false;
+			for (int i = 0; i < wordCount; i++) {
+				StringBuilder temp = new StringBuilder();
+				for (int j = 0; j < wordCount; j++) {
+					temp.append(tablet[j][i]);
+				}
+				if (!word.equals(String.valueOf(temp))) continue;
+				isWordFound = true;
+				break;
+			}
+			if (isWordFound) {
+				isValid = true;
+			} else {
+				isValid = false;
+				break;
+			}
+		}
+
+
+		return isValid;
+	}
+
+	public static String[] getWords(char[][] tablet, int wordCount) {
+		String[] names = new String[wordCount];
+		for (int i = 0; i < wordCount; i++) {
+			StringBuilder temp = new StringBuilder();
+			for (int j = 0; j < wordCount; j++) {
+				temp.append(tablet[i][j]);
+			}
+			names[i] = String.valueOf(temp);
+		}
+		return names;
+	}
+}
---
+++ src/test/java/practice/is_sator_square/KataTest.java
@@ -0,0 +1,63 @@
+package practice.is_sator_square;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class KataTest {
+	char[][][] sampleTablets = {
+			new char[][]{{'T', 'E', 'N'},
+					{'E', 'Y', 'E'},
+					{'N', 'E', 'T'}},
+
+			new char[][]{{'N', 'O', 'T'},
+					{'O', 'V', 'O'},
+					{'N', 'O', 'T'}},
+
+			new char[][]{{'B', 'A', 'T', 'S'},
+					{'A', 'B', 'U', 'T'},
+					{'T', 'U', 'B', 'A'},
+					{'S', 'T', 'A', 'B'}},
+
+			new char[][]{{'P', 'A', 'R', 'T'},
+					{'A', 'G', 'A', 'R'},
+					{'R', 'A', 'G', 'A'},
+					{'T', 'R', 'A', 'M'}},
+
+			new char[][]{{'S', 'A', 'T', 'O', 'R'},
+					{'A', 'R', 'E', 'P', 'O'},
+					{'T', 'E', 'N', 'E', 'T'},
+					{'O', 'P', 'E', 'R', 'A'},
+					{'R', 'O', 'T', 'A', 'S'}},
+
+			new char[][]{{'S', 'A', 'L', 'A', 'S'},
+					{'A', 'R', 'E', 'N', 'A'},
+					{'L', 'E', 'V', 'E', 'L'},
+					{'A', 'R', 'E', 'N', 'A'},
+					{'S', 'A', 'L', 'A', 'S'}}
+	};
+
+	void tester(char[][] tablet, boolean expected) {
+		boolean submitted = Kata.isSatorSquare(tablet);
+		StringBuilder message = new StringBuilder();
+		if(submitted != expected) {
+			message.append("Testing for tablet:\n\n");
+			for(int row = 0; row < tablet.length; row++) {
+				message.append("  ");
+				for(int col = 0; col < tablet.length; col++) {
+					message.append(tablet[row][col] + " ");
+				}
+				message.append("\n");
+			}
+			message.append("\n");
+		}
+		assertEquals(expected, submitted, message.toString());
+	}
+
+	@Test
+	void sampleTests() {
+		for(int test = 0; test < 6; test++) {
+			tester(sampleTablets[test], test % 2 == 0 ? true : false);
+		}
+	}
+}
