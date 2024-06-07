12e21f22eed8397031af395f18c94965b20ace83
JSiggelkow <J.Siggelkow@iks-gmbh.com>
2024-06-07 15:35:56 +0200

    solved transportation on vacation 8 kyu

---
+++ src/main/java/practice/kyu_8/transportation_on_vacation/Kata.java
@@ -0,0 +1,7 @@
+package practice.kyu_8.transportation_on_vacation;
+
+public class Kata {
+	public static int rentalCarCost(int d) {
+		return d >= 7 ? d * 40 - 50 : d >= 3 ? d * 40 - 20 : d * 40;
+	}
+}
