389a7d6ff927c5b7d50f3cc1267cea9f56c24ac1
sgglko <jonas.siggelkow@unitybox.de>
2024-07-01 09:27:41 +0200

    test commit

--- src/main/java/practice/kyu_8/transportation_on_vacation/Kata.java
+++ src/main/java/practice/kyu_8/transportation_on_vacation/Kata.java
@@ -2,7 +2,7 @@ package practice.kyu_8.transportation_on_vacation;
 
 public class Kata {
 	public static int rentalCarCost(int d) {
-		//test comment
+		//another test comment
 		return d >= 7 ? d * 40 - 50 : d >= 3 ? d * 40 - 20 : d * 40;
 	}
 }
